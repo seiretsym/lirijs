0faba70fd687c4a94510d15ab3dd4b53acca6f2d
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-26 10:14:59 -0700

    init files

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+
+exports.OMDB = {
+    key: process.env.OMDB_KEY
+}
+
+exports.BIT = {
+    key: process.env.BIT_KEY
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+// requires
+
+var dotenv = require("dotenv").config();
+var axios = require("axios");
+var keys = require("./keys.js");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
